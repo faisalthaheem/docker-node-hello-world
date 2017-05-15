1c8d645572cc874818c74228c8c10e20153aa611
Faisal Thaheem <faisal.ajmal@gmail.com>
2017-05-15 11:55:22 +0300

    added calls to redis for linkedin demo

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var http = require('http')
+var redis = require('redis');
+var client = redis.createClient(6379,'redis');
 
-var port = 4000
+var port = 4444
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
-})
+  
+	console.log('new request');
 
-server.listen(port)
+  var currTime;
+  
+  client.get('currenttime', function(err, data){
+	  if(err){
+		console.log('error' + err);
+		  currTime = err;
+		  return;
+	  }
+	  console.log('read data ' + data);
+	  currTime = data;
+	  response.end(data);
+  });
 
-console.log('Server running at http://localhost:' + port)
+
+	console.log('end request: ' + currTime);  
+// response.end(currTime);
+
+});
+
+server.listen(port);
+
+console.log('Server running at http://localhost:' + port);
